5b5443dd7d2a9863680fb5431518adbd5537075c
Chris-Robin Ennen <chris@ncteam.de>
2020-11-10 14:03:40 +0100

    add router

---
+++ src/views/ProductDetail.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>This is a detail page</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'product-detail',
+  props: {
+    product: Object
+  }
+}
+</script>
---
+++ src/views/ProductList.vue
@@ -0,0 +1,14 @@
+<template>
+  <section>
+    <h1>Products</h1>
+    <h2>Such nice products wow</h2>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  components: {
+  }
+}
+</script>
