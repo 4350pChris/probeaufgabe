342f2045aa469546feb44c93cbb54be3fd81a423
Chris-Robin Ennen <chris@ncteam.de>
2020-11-10 14:14:27 +0100

    add nav bar

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,16 @@
 <template>
-  <div>
+  <div style="min-height: 200vh;">
+    <NavBar />
     <div>
       <router-view />
     </div>
   </div>
 </template>
+
+<script>
+  import NavBar from "@/components/NavBar.vue";
+
+  export default {
+    components: { NavBar }
+  }
+</script>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,33 @@
+<template>
+  <nav
+      class="fixed flex w-full z-10 bg-blue-700 text-white border-b justify-center p-5 m-auto top-0 transition duration-300 ease-in-out"
+      :class="{ scrolled }"
+    >
+      Products
+    </nav>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      scrolled: false
+    }
+  },
+  beforeMount() {
+    window.addEventListener('scroll', this.handleScroll)
+  },
+  methods: {
+    handleScroll() {
+      this.scrolled = window.pageYOffset > 0;
+    }
+  }
+}
+</script>
+
+<style>
+  nav.scrolled {
+    @apply shadow-xl;
+    border-bottom: 0;
+  }
+</style>
